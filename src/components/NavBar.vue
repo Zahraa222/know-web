5678f181125bddb16e82ef8ac56f14d58c0c9bc7
Zaynab Mourtada <zaynabmo@umich.edu>
2024-09-04 20:27:53 -0400

    Adjusted navigation bar styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,9 @@
 <template>
     <nav class="navbar">
       <ul>
-        <li><a href="#">HOME</a></li>
+
+        <!--Navigation bar titles-->
+        <li><a href="#" class ="active">HOME</a></li>
         <li><a href="#">ABOUT</a></li>
         <li><a href="#">EVENTS</a></li>
         <li><a href="#">RESOURCES</a></li>
@@ -9,8 +11,11 @@
         <li><a href="#">NEWSLETTER</a></li>
         <li><a href="#">CONTACT</a></li>
       </ul>
+      <!-- Search bar-->
+      <div class="search-container">
       <input type="search" placeholder="Search" />
-    </nav>
+      <i class="search-icon"></i> 
+    </div>    </nav>
   </template>
   
   <script>
@@ -19,12 +24,16 @@
   }
   </script>
   
+  <!-- Navigation Bar Styling-->
   <style scoped>
   .navbar {
     display: flex;
     justify-content: space-between;
-    padding: 10px;
+    align-items: center;
+    padding: 10px 20px;
     background-color: #f8f8f8;
+    border: 2px solid #000;
+    font-family: 'Montserrat', sans-serif;
   }
   
   .navbar ul {
@@ -35,16 +44,41 @@
   }
   
   .navbar ul li {
-    margin-right: 15px;
+    margin-right: 30px;
   }
   
   .navbar ul li a {
     text-decoration: none;
     color: #000;
+    font-weight: bold;
+    font-size: 1.2rem;
   }
-  
+
+  /* Selected page in the navigation */
+  .navbar ul li a.active{
+    background-color: lightblue;
+    padding: 5px 10px;
+    border-radius: 5px;
+    color: black;
+  }
+
+  /* CSS styling for search bar */
+  .search-container{
+    display: flex;
+  align-items: center;
+  border: 2px solid #000;
+  border-radius: 5px;
+  padding: 2px 10px;
+  background-color: #dfe7f3;
+  box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.8);  }
+
   input[type="search"] {
-    padding: 5px;
-  }
+  padding: 5px;
+  border: none;
+  outline: none;
+  background-color: transparent;
+  font-size: 1.1rem;
+}
+
   </style>
   
